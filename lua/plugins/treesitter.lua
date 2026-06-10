daefffe93934ddc215bc00b5ecc52b2fb21b6aa7
Patrick Blood <pblood66@gmail.com>
2026-06-10 13:07:52 -0600

    update treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  init = function() 
-      local parsers = {
-          "lua", 
-          "vim",
-          "javascript",
-          "typescript",
-          "gitignore",
-          "json",
-          "html",
-      }
-  end
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "lua",
+                "vim",
+                "javascript",
+                "typescript",
+                "gitignore",
+                "json",
+                "html",
+            },
+            auto_install = true,  -- auto install when you open a file with no parser
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }

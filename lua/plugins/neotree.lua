be9e46c579939fd10cb5b0b505f2f2032c85ea4b
Patrick Blood <pblood66@gmail.com>
2026-06-10 09:38:36 -0600

    added neotree plugin

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  
+    config = function() 
+        vim.keymap.set("n", "<leader>pt", "<cmd>Neotree filesystem reveal left<CR>", { desc = "Explorer filesystem" }) 
+        vim.keymap.set("n", "<leader>gt", "<cmd>Neotree git_status<CR>", { desc = "Explorer git status" })
+        vim.keymap.set("n", "<leader>ee", "<cmd>Neotree focus<CR>", { desc = "Focus explorer" })
+    end
+    }
+}
+

981ce141f3efbf075c9c83b511c17f8dbc89c2c3
Patrick Blood <pblood66@gmail.com>
2026-06-10 15:22:05 -0600

    add barbar plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,9 +10,9 @@ return {
         lazy = false,                      -- neo-tree will lazily load itself
 
         config = function()
-            vim.keymap.set("n", "<leader>pt", "<cmd>Neotree filesystem reveal left<CR>", { desc = "Explorer filesystem" })
+            vim.keymap.set("n", "<leader>ft", "<cmd>Neotree filesystem reveal left<CR>", { desc = "Explorer filesystem" })
             vim.keymap.set("n", "<leader>gt", "<cmd>Neotree git_status<CR>", { desc = "Explorer git status" })
-            vim.keymap.set("n", "<leader>ee", "<cmd>Neotree focus<CR>", { desc = "Focus explorer" })
+            vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus<CR>", { desc = "Focus explorer" })
             vim.keymap.set("n", "<leader>ct", "<cmd>Neotree close<CR>", { desc = "Close explorer" })
         end
     }

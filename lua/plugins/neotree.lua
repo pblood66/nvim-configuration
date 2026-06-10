02d1de611490a167ed97bad14863d02e47416387
Patrick Blood <pblood66@gmail.com>
2026-06-10 13:08:20 -0600

    formatting

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,7 @@ return {
             "MunifTanjim/nui.nvim",
             "nvim-tree/nvim-web-devicons", -- optional, but recommended
         },
-        lazy = false,                -- neo-tree will lazily load itself
+        lazy = false,                      -- neo-tree will lazily load itself
 
         config = function()
             vim.keymap.set("n", "<leader>pt", "<cmd>Neotree filesystem reveal left<CR>", { desc = "Explorer filesystem" })

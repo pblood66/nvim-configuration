e6d0291ff0a7c097226f05fcf9f6b4bd909ab111
Patrick Blood <pblood66@gmail.com>
2026-06-10 11:40:28 -0600

    add lsp configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,71 +1,74 @@
 return {
-    -- Mason: installs LSP servers, linters, formatters
-    {
-        "mason-org/mason.nvim",
-        opts = {},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {}
+        })
 
-    -- Bridges mason <-> lspconfig
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = { "mason-org/mason.nvim" },
-        opts = {
-            -- List servers to auto-install here, e.g.:
-            ensure_installed = { "lua_ls", "ts_ls", "pyright" },
-        },
-    },
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
 
-    -- LSP config
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "mason-org/mason.nvim",
-            "mason-org/mason-lspconfig.nvim",
-            "saghen/blink.cmp", -- so capabilities are set up before LSP attaches
-        },
-        config = function()
-            -- Feed blink.cmp capabilities into every LSP server
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
+        require("fidget").setup({})
+        require("mason").setup()
 
-            -- Keymaps that apply when any LSP attaches to a buffer
-            vim.api.nvim_create_autocmd("LspAttach", {
-                desc = "LSP keymaps",
-                callback = function(event)
-                    local map = function(keys, func, desc)
-                        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-                    end
+        -- Set capabilities globally for all servers
+        vim.lsp.config("*", { capabilities = capabilities })
 
-                    map("gd", vim.lsp.buf.definition, "Go to Definition")
-                    map("gD", vim.lsp.buf.declaration, "Go to Declaration")
-                    map("gi", vim.lsp.buf.implementation, "Go to Implementation")
-                    map("gr", vim.lsp.buf.references, "Go to References")
-                    map("go", vim.lsp.buf.type_definition, "Go to Type Definition")
-                    map("K", vim.lsp.buf.hover, "Hover Docs")
-                    map("<C-k>", vim.lsp.buf.signature_help, "Signature Help")
-                    map("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
-                    map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-                    map("<leader>d", vim.diagnostic.open_float, "Show Diagnostics")
-                    map("[d", vim.diagnostic.goto_prev, "Prev Diagnostic")
-                    map("]d", vim.diagnostic.goto_next, "Next Diagnostic")
-                end,
-            })
-
-            -- Configure your servers here
-            local lspconfig = require("lspconfig")
-
-            -- Example: Lua
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = { completion = { callSnippet = "Replace" } },
+        -- Custom server configs
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = { version = "LuaJIT" },
+                    diagnostics = { globals = { "vim" } },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                    format = { enable = true },
                 },
-            })
+            },
+        })
 
-            -- Example: TypeScript/JavaScript
-            -- lspconfig.ts_ls.setup({ capabilities = capabilities })
+        vim.lsp.config("zls", {
+            root_markers = { ".git", "build.zig", "zls.json" },
+            settings = {
+                zls = {
+                    enable_inlay_hints = true,
+                    enable_snippets = true,
+                    warn_style = true,
+                },
+            },
+        })
+        vim.g.zig_fmt_parse_errors = 0
+        vim.g.zig_fmt_autosave = 0
 
-            -- Example: Python
-            -- lspconfig.pyright.setup({ capabilities = capabilities })
-        end,
-    },
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+                "vtsls",
+                "tailwindcss",
+            },
+            -- No handlers needed — vim.lsp.enable() is called automatically
+        })
+    end,
 }

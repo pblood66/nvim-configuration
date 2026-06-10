403f7d06946da072e711d0cd90ef5c42e684c391
Patrick Blood <pblood66@gmail.com>
2026-06-10 09:40:05 -0600

    added keybinds to move lines using alt

--- lua/pyblood/remap.lua
+++ lua/pyblood/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+
